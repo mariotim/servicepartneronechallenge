6d95bd51764805959ebd8dc479a1a36c8be67ab8
maratim <timergaliev.marat@gmail.com>
2017-08-08 18:31:35 +0200

    restructuring before going home

---
+++ src/main/java/de/storecast/workforceoptimizer/controller/WorkforceOptimizationController.java
@@ -0,0 +1,7 @@
+package de.storecast.workforceoptimizer.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class WorkforceOptimizationController {
+}
---
+++ src/main/java/de/storecast/workforceoptimizer/utils/Main.java
@@ -0,0 +1,14 @@
+package de.storecast.workforceoptimizer.utils;
+
+public class Main {
+
+    public static void test() {
+//        RoomCapacitorUtils.computeRequiredCapacity(35, 10, 6);
+//        RoomCapacitorUtils.computeRequiredCapacity(21, 10, 6);
+//        RoomCapacitorUtils.computeRequiredCapacity(17, 10, 6);
+//
+//
+//        RoomCapacitorUtils.computeRequiredCapacity(24, 11, 6);
+//        RoomCapacitorUtils.computeRequiredCapacity(28, 11, 6);
+    }
+}
